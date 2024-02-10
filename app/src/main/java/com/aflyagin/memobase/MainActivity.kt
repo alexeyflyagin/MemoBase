ddbbda08a0ec9fd938b2362ae0055a80162c9224
alexeyflyagin <alexeyflyagin06@gmail.com>
2024-02-10 14:04:07 +0200

    Added colors and theme is set up

--- app/src/main/java/com/aflyagin/memobase/MainActivity.kt
+++ app/src/main/java/com/aflyagin/memobase/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.aflyagin.memobase
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowInsetsControllerCompat
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpTheme()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun setUpTheme() {
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.TRANSPARENT
     }
 }
